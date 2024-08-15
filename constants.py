f275757f72d40cf86ef5dd199177a3d3e2507a2a
pleasewhy <1943788269@qq.com>
2024-08-15 08:28:08 +0800

    up

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ import pathlib
 import os
 
 ### Task parameters
-DATA_DIR = "/home/zfu/interbotix_ws/src/act/data" if os.getlogin() == "zfu" else "/scr/tonyzhao/datasets"
+DATA_DIR = "./dataset"
 SIM_TASK_CONFIGS = {
     "sim_transfer_cube_scripted": {
         "dataset_dir": DATA_DIR + "/sim_transfer_cube_scripted",
@@ -52,32 +52,8 @@ SIM_TASK_CONFIGS = {
 ### Simulation envs fixed constants
 DT = 0.02
 FPS = 50
-JOINT_NAMES = [
-    "waist",
-    "shoulder",
-    "elbow",
-    "forearm_roll",
-    "wrist_angle",
-    "wrist_rotate",
-]
-START_ARM_POSE = [
-    0,
-    -0.96,
-    1.16,
-    0,
-    -0.3,
-    0,
-    0.02239,
-    -0.02239,
-    0,
-    -0.96,
-    1.16,
-    0,
-    -0.3,
-    0,
-    0.02239,
-    -0.02239,
-]
+JOINT_NAMES = ["waist", "shoulder", "elbow", "forearm_roll", "wrist_angle", "wrist_rotate"]
+START_ARM_POSE = [0, -0.96, 1.16, 0, -0.3, 0, 0.02239, -0.02239, 0, -0.96, 1.16, 0, -0.3, 0, 0.02239, -0.02239]
 
 XML_DIR = str(pathlib.Path(__file__).parent.resolve()) + "/assets/"  # note: absolute path
 
